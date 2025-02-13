3b554911190f456238ef628a3bd24f52d0aaab3d
noahware <131519316+noahware@users.noreply.github.com>
2025-02-13 17:40:17 +0000

    Added subroutine hooking utils + copy memory utility in memory manager.
    Added subroutine hooking utils + copy memory utility in memory manager. We can now create inline hooks, enable them, disable them, and delete them.

--- uefi-boot/src/memory_manager/memory_manager.c
+++ uefi-boot/src/memory_manager/memory_manager.c
@@ -10,3 +10,11 @@ EFI_STATUS mm_free_pool(VOID* buffer)
 {
 	return gBS->FreePool(buffer);
 }
+
+void mm_copy_memory(UINT8* destination, UINT8* source, UINT64 size)
+{
+	for (UINT64 i = 0; i < size; i++)
+	{
+		destination[i] = source[i];
+	}
+}
