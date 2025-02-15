8fb6719acb83a0a69ec211ac51e9e1d717c346e2
noahware <131519316+noahware@users.noreply.github.com>
2025-02-15 16:33:24 +0000

    Pattern scanning + pe load hooks placed on bootmgfw.
    Pattern scanning + pe load hooks placed on bootmgfw. The hook on ImgpLoadPEImage is going to allow us to see the base addresses and sizes of modules loaded by bootmgfw. Pattern for ImgpLoadPEImage has been tested so far on 22H2, 23H2, 24H2.

--- uefi-boot/src/bootmgfw/bootmgfw.c
+++ uefi-boot/src/bootmgfw/bootmgfw.c
@@ -1,10 +1,17 @@
 #include "bootmgfw.h"
 #include "../memory_manager/memory_manager.h"
+#include "../hooks/hooks.h"
 #include "../image/image.h"
 #include "../disk/disk.h"
 
 #define d_bootmgfw_path L"\\efi\\microsoft\\boot\\bootmgfw.efi"
 #define d_path_original_bootmgfw L"\\efi\\microsoft\\boot\\bootmgfw.original.efi"
+
+#define unknown_param_t UINT64
+
+typedef UINT64(*bootmgfw_load_pe_image_t)(UINT64 a1, INT32 a2, UINT64* image_base, UINT32* image_size, UINT64* a5, UINT32* a6, UINT32* a7, UINT64 a8, UINT64 a9, unknown_param_t a10, unknown_param_t a11, unknown_param_t a12, unknown_param_t a13, unknown_param_t a14, unknown_param_t a15);
+
+hook_data_t* bootmgfw_load_pe_image_hook_data = NULL;
 
 EFI_STATUS read_original_bootmgfw_to_buffer(EFI_FILE_PROTOCOL* bootmgfw_original_file, void** buffer, UINT64 buffer_size)
 {
@@ -106,6 +113,57 @@ EFI_STATUS bootmgfw_restore_original_file(EFI_HANDLE* device_handle_out)
     return status;
 }
 
+UINT64 bootmgfw_load_pe_image_detour(UINT64 a1, INT32 a2, UINT64* image_base, UINT32* image_size, UINT64* a5, UINT32* a6, UINT32* a7, UINT64 a8, UINT64 a9, unknown_param_t a10, unknown_param_t a11, unknown_param_t a12, unknown_param_t a13, unknown_param_t a14, unknown_param_t a15)
+{
+    hook_disable(bootmgfw_load_pe_image_hook_data);
+
+    bootmgfw_load_pe_image_t original_subroutine = (bootmgfw_load_pe_image_t)bootmgfw_load_pe_image_hook_data->hooked_subroutine_address;
+
+    UINT64 return_value = original_subroutine(a1, a2, image_base, image_size, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15);
+
+    hook_enable(bootmgfw_load_pe_image_hook_data);
+
+    return return_value;
+}
+
+EFI_STATUS bootmgfw_place_load_pe_image_hook(EFI_LOADED_IMAGE* bootmgfw_image_info)
+{
+    CHAR8* code_ref_to_load_pe_image = NULL;
+
+    // ImgpLoadPEImage
+    EFI_STATUS status = scan_image(&code_ref_to_load_pe_image, bootmgfw_image_info->ImageBase, bootmgfw_image_info->ImageSize, "\x48\x89\x44\x24\x00\xE8\x00\x00\x00\x00\x8B\xD8\xEB\x00\xBB\x00\x00\x00\x00\x48\x8B", "xxxx?x????xxx?x????xx");
+
+    if (status != EFI_SUCCESS)
+    {
+        return status;
+    }
+
+    CHAR8* load_pe_image_subroutine = (code_ref_to_load_pe_image + 10) + *(UINT32*)(code_ref_to_load_pe_image + 6);
+
+    status = hook_create(&bootmgfw_load_pe_image_hook_data, load_pe_image_subroutine, (void*)bootmgfw_load_pe_image_detour);
+
+    if (status != EFI_SUCCESS)
+    {
+        return status;
+    }
+
+    return hook_enable(bootmgfw_load_pe_image_hook_data);
+}
+
+EFI_STATUS bootmgfw_place_hooks(EFI_HANDLE bootmgfw_image_handle)
+{
+    EFI_LOADED_IMAGE* bootmgfw_image_info = NULL;
+
+    EFI_STATUS status = get_image_info(&bootmgfw_image_info, bootmgfw_image_handle);
+
+    if (status != EFI_SUCCESS)
+    {
+        return status;
+    }
+
+    return bootmgfw_place_load_pe_image_hook(bootmgfw_image_info);
+}
+
 EFI_STATUS bootmgfw_run_original_image(EFI_HANDLE parent_image_handle, EFI_HANDLE device_handle)
 {
     EFI_DEVICE_PATH* device_path = NULL;
@@ -120,7 +178,16 @@ EFI_STATUS bootmgfw_run_original_image(EFI_HANDLE parent_image_handle, EFI_HANDL
 
         if (status == EFI_SUCCESS)
         {
-            status = start_image(loaded_image);
+            status = bootmgfw_place_hooks(loaded_image);
+
+            if (status == EFI_SUCCESS)
+            {
+                status = start_image(loaded_image);
+            }
+            else
+            {
+                unload_image(loaded_image);
+            }
         }
         else
         {
