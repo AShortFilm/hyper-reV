e85c9378f9f1117b026a3c216ea76f1e396d22d4
noahware <131519316+noahware@users.noreply.github.com>
2025-03-21 16:32:35 +0000

    Improved memory manager.
    Improved memory manager - added cr0 utilities (implemented ia32-doc for toggling write-protect bit without having to bitmask it out), also added use of intrinsics for copying and filling memory.

--- uefi-boot/src/memory_manager/memory_manager.c
+++ uefi-boot/src/memory_manager/memory_manager.c
@@ -2,6 +2,12 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 
+#include <intrin.h>
+
+cr0 read_cr0();
+void write_cr0(cr0 value);
+void disable_write_protection(cr0 original_cr0);
+
 EFI_STATUS mm_allocate_pages(VOID** buffer_out, UINT64 page_count, EFI_MEMORY_TYPE memory_type)
 {
 	return gBS->AllocatePages(AllocateAnyPages, memory_type, page_count, (EFI_PHYSICAL_ADDRESS*)buffer_out);
@@ -17,15 +23,45 @@ EFI_STATUS mm_free_pool(VOID* buffer)
 	return gBS->FreePool(buffer);
 }
 
-void mm_copy_memory(UINT8* destination, UINT8* source, UINT64 size)
+void mm_copy_memory(UINT8* destination, const UINT8* source, UINT64 size)
 {
-	UINT64 original_cr0 = AsmReadCr0();
-	AsmWriteCr0(original_cr0 & ~0x10000);
+	cr0 original_cr0 = read_cr0();
 
-	for (UINT64 i = 0; i < size; i++)
-	{
-		destination[i] = source[i];
-	}
+	disable_write_protection(original_cr0);
 
-	AsmWriteCr0(original_cr0);
+	__movsb(destination, source, size);
+
+	write_cr0(original_cr0);
+}
+
+void mm_fill_memory(UINT8* destination, UINT64 size, UINT8 value)
+{
+	cr0 original_cr0 = read_cr0();
+
+	disable_write_protection(original_cr0);
+
+	__stosb(destination, value, size);
+
+	write_cr0(original_cr0);
+}
+
+cr0 read_cr0()
+{
+	cr0 value = { .flags = __readcr0() };
+
+	return value;
+}
+
+void write_cr0(cr0 value)
+{
+	__writecr0(value.flags);
+}
+
+void disable_write_protection(cr0 original_cr0)
+{
+	cr0 new_cr0 = original_cr0;
+
+	new_cr0.write_protect = 0;
+
+	write_cr0(new_cr0);
 }
