abd96fb54f53d8f7057abb0af9c9897395f1ae35
noahware <131519316+noahware@users.noreply.github.com>
2025-03-21 15:57:27 +0000

    Added SLAB page allocation.
    Added SLAB page allocation. This is where our hyper-v attachment will be allocated in, and it will be protected by SLAT automatically, leaving us with less work in future to isolate it.

--- uefi-boot/src/winload/winload.c
+++ uefi-boot/src/winload/winload.c
@@ -12,6 +12,15 @@ pdpte_64* pdpt_identity_map_allocation = NULL;
 
 hook_data_t winload_load_pe_image_hook_data = { 0 };
 
+CHAR8* allocate_slab_pages = NULL;
+
+typedef UINT64(*bl_allocate_slab_pages_t)(UINT64* allocation_base_out, UINT64 pages_to_map, UINT64 a3, UINT64 a4);
+
+UINT64 winload_allocate_slab_pages(UINT64* allocation_base_out, UINT64 pages_to_map)
+{
+    return ((bl_allocate_slab_pages_t)(allocate_slab_pages))(allocation_base_out, pages_to_map, 0, 0);
+}
+
 UINT64 winload_load_pe_image_detour(bl_file_info_t* file_info, INT32 a2, UINT64* image_base, UINT32* image_size, UINT64* a5, UINT32* a6, UINT32* a7, UINT64 a8, UINT64 a9, unknown_param_t a10, unknown_param_t a11, unknown_param_t a12, unknown_param_t a13, unknown_param_t a14, unknown_param_t a15)
 {
     hook_disable(&winload_load_pe_image_hook_data);
@@ -66,6 +75,13 @@ EFI_STATUS winload_place_hooks(UINT64 image_base, UINT64 image_size)
     }
 
     status = mm_allocate_pages(&pdpt_identity_map_allocation, 1, EfiRuntimeServicesData);
+
+    if (status != EFI_SUCCESS)
+    {
+        return status;
+    }
+
+    status = scan_image(&allocate_slab_pages, (CHAR8*)image_base, image_size, "\xC1\xE9\x00\x8D\x14\x01\x48\x8B\x0D\x00\x00\x00\x00\x48\x81\xC1\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x8B\xF8\x85\xC0\x0F\x88", "xx?xxxxxx????xxx????x????xxxxxx");
 
     if (status != EFI_SUCCESS)
     {
--- uefi-boot/src/winload/winload.h
+++ uefi-boot/src/winload/winload.h
@@ -5,4 +5,5 @@
 extern pml4e_64* pml4_allocation;
 extern pdpte_64* pdpt_identity_map_allocation;
 
+UINT64 winload_allocate_slab_pages(UINT64* allocation_base_out, UINT64 pages_to_map);
 EFI_STATUS winload_place_hooks(UINT64 image_base, UINT64 image_size);
