7c279760326587152e9685bbf53abd053cc82de5
noahware <131519316+noahware@users.noreply.github.com>
2025-07-01 08:15:33 +0100

    Add load script.
    Add load script - this allows hyper-reV to be loaded at the next boot. The 'uefi-boot' and 'hyperv-attachment' files should be in the same directory as the script when it is ran so they can be placed in the EFI partition.

---
+++ load-hyper-reV.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+mountvol Z: /S
+
+set boot_directory=Z:\EFI\Microsoft\Boot\
+
+attrib -s %boot_directory%bootmgfw.efi
+move %boot_directory%bootmgfw.efi %boot_directory%bootmgfw.original.efi
+
+copy /Y %~dp0bootmgfw.efi %boot_directory%
+copy /Y %~dp0hyperv-attachment.dll %boot_directory%
+
+bcdedit /set hypervisorlaunchtype auto
+
+endlocal
+
+pause
+shutdown -r -t 0
