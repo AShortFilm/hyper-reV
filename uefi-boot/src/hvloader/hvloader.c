4aa1fa139e3e75e90b53b351d3fe77f16638c35b
noahware <131519316+noahware@users.noreply.github.com>
2025-07-08 14:50:53 +0100

    Made Hyper-V hooks more robust.
    Made Hyper-V hooks more robust. Firstly, we find the true beginning/end of the .text section by walking the Hyper-V cr3 and finding the first and last executable page around Hyper-V's entry point virtual address. We then use these boundaries to signature scan, which is safer than blindly assuming where the .text section ends or begins like we did before.
    
    In addition, we now check for the AMD signature first, as it is more uncommon, and will be far less likely to be found in an Intel Hyper-V binary.

--- uefi-boot/src/hvloader/hvloader.c
+++ uefi-boot/src/hvloader/hvloader.c
@@ -55,73 +55,150 @@ void restore_initial_hyperv_pml4e(cr3 identity_map_cr3, cr3 hyperv_cr3, pml4e_64
     hyperv_pml4[0] = initial_hyperv_pml4e;
 }
 
+// must have identity map in 0th pml4e
+UINT8 is_page_executable(cr3 cr3_to_search, virtual_address_t page)
+{
+    pml4e_64* pml4 = (pml4e_64*)(cr3_to_search.address_of_page_directory << 12);
+    pml4e_64 pml4e = pml4[page.pml4_idx];
+
+    if (pml4e.present == 0 || pml4e.execute_disable == 1)
+    {
+        return 0;
+    }
+
+    pdpte_64* pdpt = (pdpte_64*)(pml4e.page_frame_number << 12);
+    pdpte_64 pdpte = pdpt[page.pdpt_idx];
+
+    if (pdpte.present == 0 || pdpte.execute_disable == 1)
+    {
+        return 0;
+    }
+
+    if (pdpte.large_page == 1)
+    {
+        return 1;
+    }
+
+    pde_64* pd = (pde_64*)(pdpte.page_frame_number << 12);
+    pde_64 pde = pd[page.pd_idx];
+
+    if (pde.present == 0 || pde.execute_disable == 1)
+    {
+        return 0;
+    }
+
+    if (pde.large_page == 1)
+    {
+        return 1;
+    }
+
+    pte_64* pt = (pte_64*)(pde.page_frame_number << 12);
+    pte_64 pte = pt[page.pt_idx];
+
+    if (pte.present == 0 || pte.execute_disable == 1)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+UINT64 find_hyperv_text_base(cr3 hyperv_cr3, virtual_address_t entry_point)
+{
+    virtual_address_t text_address = entry_point;
+
+    while (is_page_executable(hyperv_cr3, text_address) == 1)
+    {
+        text_address.address -= 0x1000;
+    }
+
+    return text_address.address + 0x1000;
+}
+
+UINT64 find_hyperv_text_end(cr3 hyperv_cr3, virtual_address_t entry_point)
+{
+    virtual_address_t text_address = entry_point;
+
+    while (is_page_executable(hyperv_cr3, text_address) == 1)
+    {
+        text_address.address += 0x1000;
+    }
+
+    return text_address.address - 0x1000;
+}
+
 void set_up_hyperv_hooks(cr3 hyperv_cr3, virtual_address_t entry_point)
 {
     AsmWriteCr3(hyperv_cr3.flags);
 
-    UINT64 hyperv_base = entry_point.address & ~(0x200000 - 1);
+    UINT64 hyperv_text_base = find_hyperv_text_base(hyperv_cr3, entry_point);
+    UINT64 hyperv_text_end = find_hyperv_text_end(hyperv_cr3, entry_point);
+    UINT64 hyperv_text_size = hyperv_text_end - hyperv_text_base;
 
-    UINT8* hyperv_attachment_entry_point = NULL;
-
-    EFI_STATUS status = hyperv_attachment_get_relocated_entry_point(&hyperv_attachment_entry_point);
-
-    if (status == EFI_SUCCESS)
+    if (hyperv_text_base != 0)
     {
-        UINT64 size_of_image = 0x200000;
+        UINT8* hyperv_attachment_entry_point = NULL;
 
-        CHAR8* code_ref_to_vmexit_handler = NULL;
-
-        UINT8 is_intel = 1;
-
-        status = scan_image(&code_ref_to_vmexit_handler, (CHAR8*)hyperv_base, size_of_image, "\xE8\x00\x00\x00\x00\xE9\x00\x00\x00\x00\x74", "x????x????x");
-
-        if (status == EFI_NOT_FOUND)
-        {
-            status = scan_image(&code_ref_to_vmexit_handler, (CHAR8*)hyperv_base, size_of_image, "\xE8\x00\x00\x00\x00\x48\x89\x04\x24\xE9", "x????xxxxx");
-
-            is_intel = 0;
-        }
+        EFI_STATUS status = hyperv_attachment_get_relocated_entry_point(&hyperv_attachment_entry_point);
 
         if (status == EFI_SUCCESS)
         {
-            INT32 original_vmexit_handler_rva = *(INT32*)(code_ref_to_vmexit_handler + 1);
-            CHAR8* original_vmexit_handler = (code_ref_to_vmexit_handler + 5) + original_vmexit_handler_rva;
+            CHAR8* code_ref_to_vmexit_handler = NULL;
 
-            UINT8* hyperv_attachment_vmexit_handler_detour = NULL;
+            UINT8 is_intel = 0;
 
-            CHAR8* get_vmcb_gadget = NULL;
+            // search for AMD's vmexit handler
+            status = scan_image(&code_ref_to_vmexit_handler, (CHAR8*)hyperv_text_base, hyperv_text_size, "\xE8\x00\x00\x00\x00\x48\x89\x04\x24\xE9", "x????xxxxx");
 
-            if (is_intel == 0)
+            if (status == EFI_NOT_FOUND)
             {
-                status = scan_image(&get_vmcb_gadget, (CHAR8*)hyperv_base, size_of_image, "\x65\x48\x8B\x04\x25\x00\x00\x00\x00\x48\x8B\x88\x00\x00\x00\x00\x48\x8B\x81\x00\x00\x00\x00\x48\x8B", "xxxxx????xxx????xxx????xx");
+                // search for Intel's vmexit handler
+                status = scan_image(&code_ref_to_vmexit_handler, (CHAR8*)hyperv_text_base, hyperv_text_size, "\xE8\x00\x00\x00\x00\xE9\x00\x00\x00\x00\x74", "x????x????x");
 
-                if (status != EFI_SUCCESS)
-                {
-                    return;
-                }
+                is_intel = 1;
             }
-
-            UINT64 heap_physical_base = hyperv_attachment_heap_allocation_base;
-            UINT64 heap_physical_usable_base = hyperv_attachment_heap_allocation_usable_base;
-            UINT64 heap_total_size = hyperv_attachment_heap_allocation_size;
-
-            hyperv_attachment_invoke_entry_point(&hyperv_attachment_vmexit_handler_detour, hyperv_attachment_entry_point, original_vmexit_handler, heap_physical_base, heap_physical_usable_base, heap_total_size, uefi_boot_physical_base_address, uefi_boot_image_size, get_vmcb_gadget);
-
-            CHAR8* code_cave = NULL;
-
-            status = scan_image(&code_cave, (CHAR8*)hyperv_base, size_of_image, "\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC", "xxxxxxxxxxxxxxxx");
 
             if (status == EFI_SUCCESS)
             {
-                status = hook_create(&hv_vmexit_hook_data, code_cave, hyperv_attachment_vmexit_handler_detour);
+                INT32 original_vmexit_handler_rva = *(INT32*)(code_ref_to_vmexit_handler + 1);
+                CHAR8* original_vmexit_handler = (code_ref_to_vmexit_handler + 5) + original_vmexit_handler_rva;
+
+                UINT8* hyperv_attachment_vmexit_handler_detour = NULL;
+
+                CHAR8* get_vmcb_gadget = NULL;
+
+                if (is_intel == 0)
+                {
+                    status = scan_image(&get_vmcb_gadget, (CHAR8*)hyperv_text_base, hyperv_text_size, "\x65\x48\x8B\x04\x25\x00\x00\x00\x00\x48\x8B\x88\x00\x00\x00\x00\x48\x8B\x81\x00\x00\x00\x00\x48\x8B", "xxxxx????xxx????xxx????xx");
+
+                    if (status != EFI_SUCCESS)
+                    {
+                        return;
+                    }
+                }
+
+                UINT64 heap_physical_base = hyperv_attachment_heap_allocation_base;
+                UINT64 heap_physical_usable_base = hyperv_attachment_heap_allocation_usable_base;
+                UINT64 heap_total_size = hyperv_attachment_heap_allocation_size;
+
+                hyperv_attachment_invoke_entry_point(&hyperv_attachment_vmexit_handler_detour, hyperv_attachment_entry_point, original_vmexit_handler, heap_physical_base, heap_physical_usable_base, heap_total_size, uefi_boot_physical_base_address, uefi_boot_image_size, get_vmcb_gadget);
+
+                CHAR8* code_cave = NULL;
+
+                status = scan_image(&code_cave, (CHAR8*)hyperv_text_base, hyperv_text_size, "\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC", "xxxxxxxxxxxxxxxx");
 
                 if (status == EFI_SUCCESS)
                 {
-                    hook_enable(&hv_vmexit_hook_data);
+                    status = hook_create(&hv_vmexit_hook_data, code_cave, hyperv_attachment_vmexit_handler_detour);
 
-                    UINT32 new_call_rva = (UINT32)(code_cave - (code_ref_to_vmexit_handler + 5));
+                    if (status == EFI_SUCCESS)
+                    {
+                        hook_enable(&hv_vmexit_hook_data);
 
-                    mm_copy_memory(code_ref_to_vmexit_handler + 1, (UINT8*)&new_call_rva, sizeof(new_call_rva));
+                        UINT32 new_call_rva = (UINT32)(code_cave - (code_ref_to_vmexit_handler + 5));
+
+                        mm_copy_memory(code_ref_to_vmexit_handler + 1, (UINT8*)&new_call_rva, sizeof(new_call_rva));
+                    }
                 }
             }
         }
