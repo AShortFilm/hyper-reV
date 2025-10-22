36169c03fdf1d2b7f9bf4731bf0b092cd703a56b
noahware <131519316+noahware@users.noreply.github.com>
2025-10-22 15:20:38 +0100

    Made load script safer
    If the project has already been set to load, the script will not try to overwrite the boot files. This prevents overwriting the copy of the original bootmgfw.efi image.

--- load-hyper-reV.bat
+++ load-hyper-reV.bat
@@ -5,15 +5,19 @@ mountvol Z: /S
 
 set boot_directory=Z:\EFI\Microsoft\Boot\
 
-attrib -s %boot_directory%bootmgfw.efi
-move %boot_directory%bootmgfw.efi %boot_directory%bootmgfw.original.efi
+if exist "%boot_directory%bootmgfw.original.efi" (
+    echo hyper-reV seems to be already scheduled
+) else (
+    attrib -s %boot_directory%bootmgfw.efi
+    move %boot_directory%bootmgfw.efi %boot_directory%bootmgfw.original.efi
 
-copy /Y %~dp0uefi-boot.efi %boot_directory%bootmgfw.efi
-copy /Y %~dp0hyperv-attachment.dll %boot_directory%
+    copy /Y %~dp0uefi-boot.efi %boot_directory%bootmgfw.efi
+    copy /Y %~dp0hyperv-attachment.dll %boot_directory%
 
-bcdedit /set hypervisorlaunchtype auto
+    bcdedit /set hypervisorlaunchtype auto
+
+    echo hyper-reV will load at next boot
+)
 
 endlocal
-
-echo hyper-reV will load at next boot
 pause
